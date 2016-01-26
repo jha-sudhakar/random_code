a728809ba707e73b9ed0cefb69caab1e884ab698
Sudhakar Jha <jhasudhakar.ai@gmail.com>
2016-01-26 18:57:20 +0530

    backup changes

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 typedef int myint;
@@ -15,14 +16,24 @@ class BST
 {
 
 private:
+	struct node* root;
 	struct node* create_node(myint ele);
-
+	void print_inorder();
 public:
+	BST();
 	void insert(myint e);
 	bool find(const myint e);
 	bool find_min(myint& min);
 	bool find_max(myint& max);
+	void print_inorder_traversal();
 };
+
+
+BST::BST()
+{
+	root = NULL;
+}
+
 
 struct node* BST:: create_node(myint ele)
 {
@@ -34,35 +45,129 @@ struct node* BST:: create_node(myint ele)
 }
 
 
-void BST::insert(myint e)
+void BST::insert(const myint e)
 {
+	struct node* tmp = create_node(e);
+	if(NULL == root)
+	{
+		root = tmp;
+		return ;
+	}
 
-
+	struct node **cur= &root;
+	while(NULL != *cur)
+	{
+		if(*cur == NULL)
+		{
+			*cur == tmp;
+			return ;
+		} else if ((*cur)-> val >  e)
+		{
+			cur = &((*cur)->left);
+		} else
+		{
+			cur = &((*cur)->right);
+		}
+	}
 }
 
 
 bool BST::find(const myint e)
 {
-
-	return true;
+	struct node *cur= root;
+    while(NULL != cur)
+    {
+        if(cur->val == e)
+        {
+            return true;
+        } else if (cur->val >  e)
+        {
+            cur = cur->left;
+        } else
+        {
+            cur = cur->right;
+        }
+    }
+	return false;
 }
 
 bool BST::find_min(myint& min)
 {
+	if(NULL == root)
+	{
+		return false;
+	}
 
-return true;
+    struct node *cur= root, *prev= NULL;
+    while(NULL != cur)
+    {
+		prev = cur;    
+        cur = cur->left;
+    }
+	min = prev->val;
+	return true;
+
 }
 
 bool BST::find_max(myint& max)
 {
+    if(NULL == root)
+    {
+        return false;
+    }
 
-return true;
+    struct node *cur= root, *prev= NULL;
+    while(NULL != cur)
+    {
+        prev = cur;
+        cur = cur->right;
+    }
+    max = prev->val;
+
+	return true;
+}
+
+void BST::print_inorder(struct node* cur_node)
+{
+	if(cur_node == NULL)
+	{
+		return;
+	}
+	print_inorder(cur_node->left);
+	cout<<"\b " << cur_node->val;
+	print_inorder(cur_node->right);
+}
+
+void BST::print_inorder_traversal()
+{
+	cout<<"\nInorder--> ";
+	return print_inorder(root);
+	cout<<"\n
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
 
+BST obj;
+if(argc > 2)
+{
+	int i=1;
+	while(i < argc)
+	{
+		obj.inser(atoi(argv[i]);
+	}
+}
 
+//1:- Print inorder traversal.
+obj.print_inorder_traversal();
+
+//2:- Print min.
+int val= -9999; obj.find_min(val); cout<<"\n Min = " << val;
+
+//3:- Print max.
+int val= 9999; obj.find_max(val); cout<<"\n Max = " << val;
+
+cout<<"\n";
 return 0;
 };
